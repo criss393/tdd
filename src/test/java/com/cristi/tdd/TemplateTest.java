3690a2043d0b0fccd4191352d41a0f02dc86e038
Cristian Aionesa <criss@Cristians-MacBook-Pro.local>
2016-03-05 19:47:16 +0200

    adding gitignore file

--- src/test/java/com/cristi/tdd/TemplateTest.java
+++
@@ -1,7 +0,0 @@
-package com.cristi.tdd;
-
-/**
- * Created by criss on 3/5/16.
- */
-public class TestTemplate {
-    private Template template; @Before public void setUp() throws Exception { template = new Template("${one}, ${two}, ${three}"); template.set("one", "1"); template.set("two", "2"); template.set("three", "3"); } @Test public void multipleVariables() throws Exception { assertTemplateEvaluatesTo("1, 2, 3"); }
---
+++ src/test/java/com/cristi/tdd/TestTemplate.java
@@ -0,0 +1,39 @@
+package com.cristi.tdd;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by criss on 3/5/16.
+ */
+public class TestTemplate {
+    private Template template;
+
+    @Before
+    public void setUp() throws Exception {
+        template = new Template("${one}, ${two}, ${three}");
+        template.set("one", "1");
+        template.set("two", "2");
+        template.set("three", "3");
+    }
+
+    @Test
+    public void multipleVariables() throws Exception {
+        assertTemplateEvaluatesTo("1, 2, 3");
+    }
+
+    @Test
+    public void unknownVariablesAreIgnored() throws Exception {
+        template.set("doesnotexist", "whatever");
+        assertTemplateEvaluatesTo("1, 2, 3");
+    }
+
+    private void assertTemplateEvaluatesTo(String expected) {
+        assertEquals(expected, template.evaluate());
+    }
+}
+
+
+
