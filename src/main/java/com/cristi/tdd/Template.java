5f7e2d385f26fbf9fdaa1e6cfc5707d38e91708b
Cristian Aionesa <criss@Cristians-MacBook-Pro.local>
2016-03-05 21:00:32 +0200

    adding first test+source

---
+++ src/main/java/com/cristi/tdd/Template.java
@@ -0,0 +1,16 @@
+package com.cristi.tdd;
+
+/**
+ * Created by criss on 3/5/16.
+ */
+public class Template {
+    public Template(String s) {
+    }
+
+    public void set(String one, String s) {
+    }
+
+    public String evaluate() {
+        return null;
+    }
+}
